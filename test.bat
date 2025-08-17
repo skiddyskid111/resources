972bed52f52dcfbb8c3484ba212d740a37d6e442
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-08-17 11:43:38 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,6 +1,8 @@
-@echo off
-set WEBHOOK_URL=https://discord.com/api/webhooks/1406275854169804891/ajy1l1F8b6tVNEV0_zB_SMQumHG82o54uOavEq-_HQE4Yyp7cu39OtF7IxVNLTQfqeRV
-set MESSAGE=Hello
+$WebhookUrl = "https://discord.com/api/webhooks/1406275854169804891/ajy1l1F8b6tVNEV0_zB_SMQumHG82o54uOavEq-_HQE4Yyp7cu39OtF7IxVNLTQfqeRV"
+$Message = "Hello"
 
-curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"%MESSAGE%\"}" %WEBHOOK_URL%
-pause
+$Payload = @{
+    content = $Message
+} | ConvertTo-Json
+
+Invoke-RestMethod -Uri $WebhookUrl -Method Post -ContentType "application/json" -Body $Payload
