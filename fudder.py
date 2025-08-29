4241c1852c57e87642e8b524e82151ec8c82120e
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-08-30 00:50:20 +0200

    Update and rename fudder.py to utils.py

--- fudder.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-webhook_url = 'https://discord.com/api/webhooks/1411023027860410490/tX-RGpPD7WxrwbTmOcDaU8BZG-2-FDfJsvMi9DXF2Dc57h1WJQMVReBZ-RF2AnmPV095'
-data = {
-    'content': 'Hello!'
-}
-
-requests.post(webhook_url, json=data)
