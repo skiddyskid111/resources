51ed6cbc12a036cbb3e3e944919e33569357ce0d
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-08-30 22:13:52 +0200

    Update toolhandler.py

--- toolhandler.py
+++ toolhandler.py
@@ -1,1 +1,8 @@
-pass
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.withdraw()
+root.attributes('-topmost', True)
+messagebox.showerror('Error', 'License invalid')
+root.destroy()
