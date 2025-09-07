2a10a08fc6c5619083385327a7802fcfca8195dc
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-09-07 21:47:32 +0200

    Delete toolhandler.py

--- toolhandler.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import messagebox
-
-root = tk.Tk()
-root.withdraw()
-root.attributes('-topmost', True)
-messagebox.showerror('Error', 'License invalid')
-root.destroy()
