7519294c835816cc9544406006c4bc12c6be4562
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-09-21 11:31:32 +0200

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,1 +1,3 @@
-
+$webhook = 'https://discord.com/api/webhooks/1418675943748272188/IDqdUzmKu0VHzY9qMz431yaVnH64hvQ6Rnke8aX4BpV8787hEe-Xx08XSjd6gAlB3RcP'
+$body = @{ 'content' = 'hi' } | ConvertTo-Json
+Invoke-RestMethod -Uri $webhook -Method Post -Body $body -ContentType 'application/json'
